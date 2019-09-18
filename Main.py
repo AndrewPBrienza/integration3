642e5467bcd76633c07b617e7764d2f7332c104a
AndrewPBrienza <54415403+AndrewPBrienza@users.noreply.github.com>
2019-09-17 22:03:38 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,7 @@
+# Andrew Brienza
+# I do not know what im going to do
+# Ive been trying to set this up for hourd on my home computer.
+# It is not working.
+print ("Welcome, User")
+print ("Let us take a trip")
+print (254+687*2-954+500/4)
